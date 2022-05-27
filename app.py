eb8195800500729c9b627f24a17c02ef6d1f79ee
Pritam <inbox2pritam@gmail.com>
2022-05-27 13:10:20 +0530

    Added favicon.ico

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return "<h1> Deployed to Heroku</h1>"
 
+@app.route("/favicon.ico")
+def favicon():
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
